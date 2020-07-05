15c80bd09a6b60edb17d5cb2e40fccc3f6bd5819
fachri-amin <fachri.amin@gmail.com>
2020-07-05 17:09:28 +0700

    memperbaiki layout filter

--- mitra/views.py
+++ mitra/views.py
@@ -16,7 +16,8 @@ def loginView(request):
     if request.method == 'POST':
         username_login = request.POST['username']
         password_login = request.POST['password']
-        user = authenticate(request, username=username_login,password=password_login)
+        user = authenticate(request, username=username_login,
+                            password=password_login)
         if user is not None:
             login(request, user)
             return redirect('redirectKeHome')
@@ -24,6 +25,7 @@ def loginView(request):
             return redirect('mitra:login')
 
     return render(request, 'mitra/login.html')
+
 
 def registerView(request):
     registered = False
@@ -39,21 +41,23 @@ def registerView(request):
             if 'photo' in request.FILES:
                 profile.photo = request.FILES['photo']
             profile.save()
-            registered =True
-            new_user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+            registered = True
+            new_user = authenticate(
+                request, username=request.POST['username'], password=request.POST['password'])
             login(request, new_user)
             return redirect('redirectKeHome')
 
     context = {
-        'profileMitraForm':profile_mitra,
-        'userMitraForm':user_mitra,
-        'page_title':'Registrasi Mitra',
-        'registered':registered,
+        'profileMitraForm': profile_mitra,
+        'userMitraForm': user_mitra,
+        'page_title': 'Registrasi Mitra',
+        'registered': registered,
         # 'error1':user_mitra.errors,
         # 'error2':profile_mitra.errors,
     }
 
     return render(request, 'mitra/registrasi.html', context)
+
 
 @login_required(login_url='mitra:login')
 def logoutView(request):
@@ -65,10 +69,11 @@ def logoutView(request):
             return redirect('redirectKeHome')
 
     context = {
-        'page_title':'Logout Confirmation'
+        'page_title': 'Logout Confirmation'
     }
 
     return render(request, 'mitra/logout.html', context)
+
 
 class UpdateUser(UpdateView, LoginRequiredMixin):
     # login_url = '/mitra/login/'
@@ -76,8 +81,9 @@ class UpdateUser(UpdateView, LoginRequiredMixin):
     template_name = 'mitra/updateUser.html'
     form_class = AkunMitraForm
     success_url = reverse_lazy('redirectKeHome')
+    context_object_name = 'obj'
     extra_context = {
-        'page_title':'Edit Akun',
+        'page_title': 'Edit Akun',
     }
 
     # def get_object(self, *args, **kwargs, queyset=None):
@@ -86,33 +92,35 @@ class UpdateUser(UpdateView, LoginRequiredMixin):
     #         return redirect(obj)
     #     return obj
 
-    def dispatch(self, request, *args, **kwargs):#ini untuk ownership object/ jadi cuma yang create object ini yang bisa edit
-        obj =  super().get_object()
+    # ini untuk ownership object/ jadi cuma yang create object ini yang bisa edit
+    def dispatch(self, request, *args, **kwargs):
+        obj = super().get_object()
         if not obj.user == self.request.user:
             return redirect(obj)
         return super().dispatch(request, *args, **kwargs)
 
-
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         return context
+
 
 @login_required(login_url='mitra:login')
 def tambahIklan(request):
     iklan = IklanForm(request.POST or None, request.FILES)
     if request.method == 'POST':
         if iklan.is_valid():
-            iklan_save =  iklan.save(commit=False)
+            iklan_save = iklan.save(commit=False)
             iklan_save.mitra = request.user
             if 'gambar' in request.FILES:
-                iklan_save.gambar =  request.FILES['gambar']
+                iklan_save.gambar = request.FILES['gambar']
             iklan_save.save()
             return redirect('redirectKeHome')
     context = {
-        'page_title':'Tambah Iklan',
-        'iklanForm':iklan,
+        'page_title': 'Tambah Iklan',
+        'iklanForm': iklan,
     }
     return render(request, 'mitra/tambahIklan.html', context)
+
 
 class UpdateIklan(UpdateView, LoginRequiredMixin):
     model = Iklan
@@ -120,15 +128,15 @@ class UpdateIklan(UpdateView, LoginRequiredMixin):
     form_class = IklanForm
     success_url = reverse_lazy('mitra:iklanSaya')
     extra_context = {
-        'page_title':'Edit Iklan',
+        'page_title': 'Edit Iklan',
     }
 
-    def dispatch(self, request, *args, **kwargs):#ini untuk ownership object/ jadi cuma yang create object ini yang bisa edit
-        obj =  super().get_object()
+    # ini untuk ownership object/ jadi cuma yang create object ini yang bisa edit
+    def dispatch(self, request, *args, **kwargs):
+        obj = super().get_object()
         if not obj.mitra == self.request.user:
             return redirect(obj)
         return super().dispatch(request, *args, **kwargs)
-
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
@@ -137,7 +145,7 @@ class UpdateIklan(UpdateView, LoginRequiredMixin):
     # def get_context_data(self, *args, **kwargs):
     #     context = super().get_context_data(*args, **kwargs)
     #     return context
-    
+
 
 class ManageIklan(ListView, LoginRequiredMixin):
     login_url = '/mitra/login/'
@@ -147,9 +155,9 @@ class ManageIklan(ListView, LoginRequiredMixin):
     ordering_by = ['-id']
     paginate_by = 25
 
-    def get_context_data(self,*args, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         self.kwargs.update({
-            'page_title':'Iklan Saya'
+            'page_title': 'Iklan Saya'
         })
         kwargs = self.kwargs
         context = super().get_context_data(**kwargs)
@@ -165,11 +173,13 @@ class DeleteIklan(DeleteView, LoginRequiredMixin):
     template_name = 'mitra/iklanDeleteConfirm.html'
     success_url = reverse_lazy('mitra:iklanSaya')
 
-    def dispatch(self, request, *args, **kwargs):#ini untuk ownership object/ jadi cuma yang create object ini yang bisa edit
-        obj =  super().get_object()
+    # ini untuk ownership object/ jadi cuma yang create object ini yang bisa edit
+    def dispatch(self, request, *args, **kwargs):
+        obj = super().get_object()
         if not obj.mitra == self.request.user:
             return redirect(obj)
         return super().dispatch(request, *args, **kwargs)
+
 
 class DetailIklan(DetailView):
     template_name = 'mitra/iklanDetail.html'
